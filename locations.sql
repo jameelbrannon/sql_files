e47fda4db44e99ea81b27d894c62052c2d8613fc
jameelbrannon <117498320+jameelbrannon@users.noreply.github.com>
2024-01-31 11:00:15 -0800

    Delete locations.sql

--- locations.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: select * from LOCATIONS
-LIMIT 0, 1000
-
--- Date: 2024-01-31 10:36
-*/
-INSERT INTO `` (`LOCT_ID`,`DEP_ID_LOC`) VALUES ('L0001','2');
-INSERT INTO `` (`LOCT_ID`,`DEP_ID_LOC`) VALUES ('L0002','5');
-INSERT INTO `` (`LOCT_ID`,`DEP_ID_LOC`) VALUES ('L0003','7');
