f768e1281b6b2a36b4df282058e6c8720ca84432
jameelbrannon <117498320+jameelbrannon@users.noreply.github.com>
2024-01-31 12:45:51 -0800

    change column new after function use

---
+++ employee database(sql)/change_col_name.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select dep_id, count(*) as "NUM_EMPLOYEES", avg(salary) as "AVG_SALARY"
+from employees
+group by dep_id
+LIMIT 0, 1000
+
+-- Date: 2024-01-31 12:43
+*/
+INSERT INTO `` (`dep_id`,`NUM_EMPLOYEES`,`AVG_SALARY`) VALUES ('2',3,86666.666667);
+INSERT INTO `` (`dep_id`,`NUM_EMPLOYEES`,`AVG_SALARY`) VALUES ('5',4,65000.000000);
+INSERT INTO `` (`dep_id`,`NUM_EMPLOYEES`,`AVG_SALARY`) VALUES ('7',3,66666.666667);
