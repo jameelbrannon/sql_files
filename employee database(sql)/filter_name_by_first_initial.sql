a8b66a8a5cf4a7db411d4a82a1f30bd266c07c2f
jameelbrannon <117498320+jameelbrannon@users.noreply.github.com>
2024-01-31 12:48:37 -0800

    filter name by first initial only

---
+++ employee database(sql)/filter_name_by_first_initial.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: select f_name, l_name
+from employees
+where f_name like 'S%'
+LIMIT 0, 1000
+
+-- Date: 2024-01-31 12:48
+*/
+INSERT INTO `` (`f_name`,`l_name`) VALUES ('Steve','Wells');
+INSERT INTO `` (`f_name`,`l_name`) VALUES ('Santosh','Kumar');
