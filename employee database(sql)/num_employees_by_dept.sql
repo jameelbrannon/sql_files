6dcb6b606eb4c56d87c879601b627fcddaa03fe3
jameelbrannon <117498320+jameelbrannon@users.noreply.github.com>
2024-01-31 12:38:23 -0800

    count number of employees by dept

---
+++ employee database(sql)/num_employees_by_dept.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select dep_id, count(*)
+from employees
+group by dep_id
+LIMIT 0, 1000
+
+-- Date: 2024-01-31 12:38
+*/
+INSERT INTO `` (`dep_id`,`count(*)`) VALUES ('2',3);
+INSERT INTO `` (`dep_id`,`count(*)`) VALUES ('5',4);
+INSERT INTO `` (`dep_id`,`count(*)`) VALUES ('7',3);
