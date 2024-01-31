6784034f6d6b41363c27d758b8dfd906430de90a
jameelbrannon <117498320+jameelbrannon@users.noreply.github.com>
2024-01-31 12:55:27 -0800

    group by salary

---
+++ employee database(sql)/groupBy_salary.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: select dep_id, avg(salary) as "AVG_SALARY"
+from employees
+group by dep_id
+having avg(salary) >= 60000
+LIMIT 0, 1000
+
+-- Date: 2024-01-31 12:54
+*/
+INSERT INTO `` (`dep_id`,`AVG_SALARY`) VALUES ('2',86666.666667);
+INSERT INTO `` (`dep_id`,`AVG_SALARY`) VALUES ('5',65000.000000);
+INSERT INTO `` (`dep_id`,`AVG_SALARY`) VALUES ('7',66666.666667);
