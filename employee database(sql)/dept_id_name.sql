880692c84e4373ba3e7d54b0332d9303344eca30
jameelbrannon <117498320+jameelbrannon@users.noreply.github.com>
2024-01-31 12:34:57 -0800

    order by dept it and name

---
+++ employee database(sql)/dept_id_name.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: select f_name, l_name, dep_id
+FROM EMPLOYEES order by DEP_ID desc, l_name desc
+LIMIT 0, 1000
+
+-- Date: 2024-01-31 12:33
+*/
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('Mary','Thomas','7');
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('Andrea','Jones','7');
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('Bharath','Gupta','7');
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('Steve','Wells','5');
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('Santosh','Kumar','5');
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('Alice','James','5');
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('Ann','Jacob','5');
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('John','Thomas','2');
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('Ahmed','Hussain','2');
+INSERT INTO `` (`f_name`,`l_name`,`dep_id`) VALUES ('Nancy','Allen','2');
