65e0f5021df367fecd135acc84cf429bd7f7550d
jameelbrannon <117498320+jameelbrannon@users.noreply.github.com>
2024-01-31 12:24:22 -0800

    get all people  born in 1970s

---
+++ employee database(sql)/birth_dates.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: select F_NAME, L_NAME
+FROM EMPLOYEES
+WHERE B_DATE LIKE '197%'
+LIMIT 0, 1000
+
+-- Date: 2024-01-31 12:23
+*/
+INSERT INTO `` (`F_NAME`,`L_NAME`) VALUES ('John','Thomas');
+INSERT INTO `` (`F_NAME`,`L_NAME`) VALUES ('Alice','James');
+INSERT INTO `` (`F_NAME`,`L_NAME`) VALUES ('Nancy','Allen');
+INSERT INTO `` (`F_NAME`,`L_NAME`) VALUES ('Mary','Thomas');
