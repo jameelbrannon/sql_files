661d9894a892cd37aaba2522d813c129caa64e0a
jameelbrannon <117498320+jameelbrannon@users.noreply.github.com>
2024-01-31 11:00:49 -0800

    Delete departments.sql

--- departments.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: select * from DEPARTMENTS
-LIMIT 0, 1000
-
--- Date: 2024-01-31 10:35
-*/
-INSERT INTO `` (`DEPT_ID_DEP`,`DEP_NAME`,`MANAGER_ID`,`LOC_ID`) VALUES ('2','Architect Group','30001','L0001');
-INSERT INTO `` (`DEPT_ID_DEP`,`DEP_NAME`,`MANAGER_ID`,`LOC_ID`) VALUES ('5','Software Group','30002','L0002');
-INSERT INTO `` (`DEPT_ID_DEP`,`DEP_NAME`,`MANAGER_ID`,`LOC_ID`) VALUES ('7','Design Team','30003','L0003');
